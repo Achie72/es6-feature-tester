ccda528427c85900a95b47c6f6fe579f4910105e
Bela <tbela@inf.u-szeged.hu>
2019-03-07 17:39:41 +0100

    Replace console.log with print for compatibility

--- features/extended_literals/unicode_string_and_regexp_literal.js
+++ features/extended_literals/unicode_string_and_regexp_literal.js
@@ -1,7 +1,7 @@
 var assert = function (a) { if (!a) { throw Error("Assertion Failed")}}
-console.log("𠮷".length === 2)
-console.log("𠮷".match("/./u")[0].length === 2)
-console.log("𠮷" === "\uD842\uDFB7")
-console.log("𠮷" === "\u{20BB7}")
-console.log("𠮷".codePointAt(0) == 0x20BB7)
+assert("𠮷".length === 2)
+assert("𠮷".match("/./u")[0].length === 2)
+assert("𠮷" === "\uD842\uDFB7")
+assert("𠮷" === "\u{20BB7}")
+assert("𠮷".codePointAt(0) == 0x20BB7)
 for (var codepoint of "𠮷") print(codepoint)
